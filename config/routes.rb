c9303afb0ae3b0937bcd3bbd99613b877db31e56
TYIRI <TYIRI@MacBook.local>
2021-03-15 18:39:20 +0900

    ユーザー作成機能とログインを実装 #5

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
   root 'recipes#index'
 end
