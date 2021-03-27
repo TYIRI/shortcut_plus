c479c2ab7217d9c07bfca4ff7e8de182f716a3f6
TYIRI <tkurihara1987@gmail.com>
2021-03-27 11:13:27 +0900

    [Add]タグごとのレシピ表示機能を実装 #57

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     end
   end
   resources :categories, only: %i[show]
+  resources :tags, only: %i[show]
   resources :recipes, shallow: true, only: %i[new create show edit update destroy] do
     resources :comments, only: %i[create destroy]
     get 'get_tag_search', on: :collection, defaults: { format: 'json' }
@@ -18,7 +19,7 @@ Rails.application.routes.draw do
     get '/', to: 'users#edit', as: :settings
     resources :email_changes, only: %i[new create edit]
   end
-  # get '/settings/email_change', to: 'users#email_change'
+
   get '/my_recipes', to: 'recipes#my_recipes'
   get '/search', to: 'recipes#search'
   get '/login', to: 'sessions#new'
