01b15a6de39412b73991f9fe7e98f77a5a175c79
TYIRI <tkurihara1987@gmail.com>
2021-03-29 18:21:30 +0900

    [Add]コメントの投稿とLike機能をAjax化 #63

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -1,13 +1,11 @@
 class CommentLikesController < ApplicationController
   def create
-    comment = Comment.find(params[:comment_id])
-    current_user.like_comment(comment)
-    redirect_to comment.recipe
+    @comment = Comment.find(params[:comment_id])
+    current_user.like_comment(@comment)
   end
-
+  
   def destroy
-    comment = Comment.find(params[:id])
-    current_user.dislike_comment(comment)
-    redirect_to comment.recipe
+    @comment = Comment.find(params[:id])
+    current_user.dislike_comment(@comment)
   end
 end
