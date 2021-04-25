3a721f2d701ea16f792b65649d7d320cd04a5814
TYIRI <tkurihara1987@gmail.com>
2021-04-25 23:57:18 +0900

    [Add]例外処理を実装 #89

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,36 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :require_login
 
+  rescue_from StandardError, with: :error_500
+  rescue_from NoMemoryError, ScriptError, Interrupt, SecurityError, SignalException, SystemStackError, with: :only_logging
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404
+
   protected
 
   def not_authenticated
     redirect_to login_path
   end
 
+  private
+
   def set_categories
     @categories = Category.all
+  end
+
+  def error_404(error = nil)
+    logger.error error.message if error
+    logger.error error.backtrace.join("\n\n") if error
+    render file: Rails.root.join('public', '404.html'), layout: false, status: :not_found
+  end
+
+  def error_500(error = nil)
+    logger.error error.message if error
+    logger.error error.backtrace.join("\n\n") if error
+    render file: Rails.root.join('public', '500.html'), layout: false, status: :internal_server_error
+  end
+  
+  def only_logging(error = nil)
+    logger.error error.message if error
+    logger.error error.backtrace.join("\n\n") if error
   end
 end
